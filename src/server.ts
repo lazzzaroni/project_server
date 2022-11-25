efd774aa62e6c3ee8e7768420474801bc873a24c
Mikalai Paliakou <mpaliakou@exadel.com>
2022-11-25 13:52:50 +0100

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
+import cors from "cors";
 import express, { Express, NextFunction, Request, Response } from "express";
 import about from "./routes/about";
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
