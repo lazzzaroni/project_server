a855a5b4d6c3918c9af1e2c25416be8d1314dd70
Mikalai Paliakou <mpaliakou@exadel.com>
2022-11-25 18:34:59 +0100

    feat: add User model

---
+++ src/models/User.ts
@@ -0,0 +1,44 @@
+import { model, Schema } from "mongoose";
+
+interface IUser {
+  name: String;
+  email: IEmail;
+  password: String;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface IEmail {
+  type: String;
+  lowercase: boolean;
+  unique: boolean;
+  trim: boolean;
+  required: boolean;
+  validate: {
+    validator: Function;
+  };
+}
+
+const UserSchema = new Schema<IUser>({
+  name: String,
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    trim: true,
+    required: [true, "Cannot create user without an email."],
+    validate: {
+      validator: (value: string): boolean => {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      },
+      message: "{VALUE} is not a valid email!",
+    },
+  },
+  password: { type: String, required: true, minlength: 8 },
+  createdAt: { type: Date, immutable: true, default: () => Date.now() },
+  updatedAt: { type: Date, default: () => Date.now() },
+});
+
+const UserModel = model<IUser>("User", UserSchema);
+
+export default UserModel;
