170b16cdecfefd4ce254c2cecdf51efc39308181
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-02 13:58:13 +0100

    refactor: auth logic

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,12 +1,12 @@
 import { NextFunction, Request, Response } from "express";
-import User, { IUser } from "../models/User";
+import User from "../models/User";
 import { comparePasswords, createJWT, hashPassword } from "../services/auth";
 
-export async function createNewUser(
+export const createNewUser = async (
   req: Request,
   res: Response,
   next: NextFunction
-) {
+) => {
   try {
     const newUser = new User({
       name: req.body.name,
@@ -17,13 +17,16 @@ export async function createNewUser(
     const token = createJWT(newUser);
     const createdUser = await newUser.save();
     res.json({ createdUser, token });
-  } catch (err: any) {
-    res.status(400).json({ message: `${err.message}` });
-    next(err);
+  } catch (e) {
+    console.error(e);
+    res.status(400).json({
+      message: "User with specified email address already exists",
+    });
+    next(e);
   }
-}
+};
 
-export async function loginUser(req: Request, res: Response) {
+export const loginUser = async (req: Request, res: Response) => {
   const user = await User.findOne({
     id: req.body.id,
     email: req.body.email,
@@ -32,7 +35,7 @@ export async function loginUser(req: Request, res: Response) {
 
   const isValid = await comparePasswords(
     req.body.password,
-    user!.password as string
+    user.password as string
   );
   if (!isValid) {
     return res.status(400).json({ message: "User is not valid" });
@@ -40,4 +43,4 @@ export async function loginUser(req: Request, res: Response) {
 
   const token = createJWT(user);
   res.json({ token });
-}
+};
