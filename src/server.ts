d09bc87195b3b35da0c56d22837974a9b6b4a1a5
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-02 13:21:42 +0100

    feat: add morgan

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import cors from "cors";
 import express, { Express, NextFunction, Request, Response } from "express";
+import morgan from "morgan";
 import { createNewUser, loginUser } from "./controllers/auth.controller";
 import router from "./router";
 
 const app: Express = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
