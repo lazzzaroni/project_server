f0340f97e7a0f0381d14e49a5580eb3a276af944
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-06 20:00:02 +0100

    refactor: rename controller

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUsers } from "./controllers/users.controller";
+import { getUsers } from "./controllers/user.controller";
 
 const router = Router();
 
