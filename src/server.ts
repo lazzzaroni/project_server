1ad738fae43e2c71a3e9fa505fcf54899579156d
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-06 20:00:44 +0100

    feat: add simple error handler

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Express, NextFunction, Request, Response } from "express";
 import morgan from "morgan";
+import config from "./config";
 import { createNewUser, loginUser } from "./controllers/auth.controller";
 import router from "./router";
 import { protect } from "./services/auth";
@@ -21,5 +22,17 @@ app.use("/api", protect, router);
 
 app.post("/signup", createNewUser);
 app.post("/signin", loginUser);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.log("Middleware Error Handling");
+  const errStatus = err.statusCode || 500;
+  const errMsg = err.message || "Something went wrong";
+  res.status(errStatus).json({
+    success: false,
+    status: errStatus,
+    message: errMsg,
+    stack: config.env === "development" ? err.stack : {},
+  });
+});
 
 export default app;
