679c48a0f91e93ea24b45c429dcdbeb67154e02e
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-13 14:41:40 +0100

    feat: add middleware for uploading images

---
+++ src/middleware/ImageUpload.ts
@@ -0,0 +1,42 @@
+import multer from "multer";
+import path from "path";
+
+interface IMulterSingle {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  destination: string;
+  filename: string;
+  path: string;
+  size: number;
+}
+
+const storageEngine = multer.diskStorage({
+  destination: "./images",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage: storageEngine,
+  limits: { fileSize: 10000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+});
+
+const checkFileType = function (file: IMulterSingle, cb: any) {
+  const fileTypes = /jpeg|jpg|png|gif|svg/;
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Only Images Allowed");
+  }
+};
+
+export default upload;
