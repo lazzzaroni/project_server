8abd6023797f04d07a89a434edaddcd50f1e8a64
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-13 14:42:03 +0100

    feat: add controller for images

---
+++ src/controllers/images.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+export async function uploadSingleFile(req: Request, res: Response) {
+  if (req.file) {
+    console.log(req.file);
+    res.send("Single file uploaded successfully");
+  } else {
+    res.status(400).send("Please upload a valid image");
+  }
+}
+
+export async function uploadMultipleFiles(req: Request, res: Response) {
+  if (req.files) {
+    console.log(req.files);
+    res.send("Multiple files uploaded successfully");
+  } else {
+    res.status(400).send("Please upload a valid images");
+  }
+}
