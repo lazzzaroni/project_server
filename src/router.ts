b9dae1bdb47dd5c815f8e6d614dc61146663c99f
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-13 14:42:46 +0100

    feat: add POST for image uploading

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
+import {
+  uploadMultipleFiles,
+  uploadSingleFile,
+} from "./controllers/images.controller";
 import { getUsers } from "./controllers/user.controller";
+import upload from "./middleware/ImageUpload";
 
 const router = Router();
 
 router.get("/users", getUsers);
+
+router.post("/images/single", upload.single("image"), uploadSingleFile);
+router.post("/images/multiple", upload.array("images", 5), uploadMultipleFiles);
 
 export default router;
