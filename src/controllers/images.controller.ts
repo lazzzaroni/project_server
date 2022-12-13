6e3769e9bfe1cb3c087417f836dac4a698a20f18
Mikalai Paliakou <mpaliakou@exadel.com>
2022-12-13 14:43:31 +0100

    refactor: remove console.log from code

--- src/controllers/images.controller.ts
+++ src/controllers/images.controller.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 
 export async function uploadSingleFile(req: Request, res: Response) {
   if (req.file) {
-    console.log(req.file);
     res.send("Single file uploaded successfully");
   } else {
     res.status(400).send("Please upload a valid image");
@@ -11,7 +10,6 @@ export async function uploadSingleFile(req: Request, res: Response) {
 
 export async function uploadMultipleFiles(req: Request, res: Response) {
   if (req.files) {
-    console.log(req.files);
     res.send("Multiple files uploaded successfully");
   } else {
     res.status(400).send("Please upload a valid images");
